6ef530dc3cd29f8762f5d9f96f88301217465a32
gordon.aspin@gmail.com <gordon.aspin@gmail.com>
2024-12-23 13:19:15 -0600

    sorted results

--- quartiles.py
+++ quartiles.py
@@ -19,20 +19,25 @@ def main():
     els = str(input("enter all fragments separated by spaces:")).lower().split()
 
     with open(r'words_alpha.txt', 'r') as fp:
-        words = fp.read().splitlines()
+        dictionary_words = fp.read().splitlines()
 
     matches = 0
     for i in range(1,5):
-        sub_matches = 0
-        print(f"{i} " + "_"*i)
+        words = []
         permutations = list(itertools.permutations(els, i))
         for perm in permutations:
             word = "".join(perm)
-            if binary_search(words, word) != -1:
-                sub_matches = sub_matches + 1
-                matches = matches + 1
-                print(f"{sub_matches}: {word}")
+            if binary_search(dictionary_words, word) != -1:
+                words.append(word)
+                #print(f"{sub_matches}: {word}")
+        words.sort()
+        matches = matches + print_words(i, matches, words)
 
-    print(matches)
+def print_words(i, matches, wordlist):
+    print(f"{i} " + "_"*i)
+    for word in wordlist:
+        matches = matches + 1
+        print(f"{matches}: {word}")
+    return len(wordlist)
 
 main()
